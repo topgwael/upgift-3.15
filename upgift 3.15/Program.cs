71c6eab8e271c1aae2daa6498dd4da780a31efba
Pingvu <nhlpool27@gmail.com>
2022-10-06 13:41:27 +0200

    Add project files.

---
+++ upgift 3.15/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+namespace Uppgift_3_15
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hur mång timmer vill du hyra en bil?");
+            string x = Console.ReadLine();
+            int y = int.Parse(x);
+            int z = y * 80;
+            if (y >= 12)
+            {
+                Console.WriteLine("Du kan inte hyra bilen så länge.");
+            }
+            else
+            {
+                Console.WriteLine("Det kommer kosta " + z + " kronor");
+            }
+        }
+    }
+}
